d70df531bb16a5c377951fb99c9e6b3ea083cb1d
Prasad Kute <prasadkute0708@gmail.com>
2025-02-15 15:04:54 +0530

    donor and ngo routes updated

---
+++ web/src/pages/donor/DonorHistory.tsx
@@ -0,0 +1,7 @@
+export default function DonorHistory () {
+    return (
+        <div>
+        <h1>DonorHistory</h1>
+        </div>
+    );
+}
---
+++ web/src/pages/ngo/NgoHistory.tsx
@@ -0,0 +1,7 @@
+export default function NgoHistory () {
+    return (
+        <div>
+        <h1>NGO History</h1>
+        </div>
+    );
+}
